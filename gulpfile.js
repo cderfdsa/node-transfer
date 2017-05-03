c9b41a0be1b0f90d480eb2b9d22148498c14ec04
zhanglingfe <771152260@qq.com>
2017-05-03 12:23:01 +0800

    gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,105 +6,100 @@ var path = require("path");
 var uglify = require('gulp-uglify');
 var rename = require("gulp-rename");
 
+// 清空 dist
 gulp.task("clean",function(){
-  var stream = gulp.src("dist")
-  .pipe(clean());
-  return stream;
+	var stream = gulp.src("dist")
+	.pipe(clean());
+	return stream;
 });
 
 gulp.task("copyBin",function(){
-  var stream = gulp.src("src/bin/**/*")
-  .pipe(gulp.dest('dist/bin'));
-  return stream;
+	var stream = gulp.src("src/bin/**/*")
+	.pipe(gulp.dest('dist/bin'));
+	return stream;
 });
 gulp.task("copyConfig",function(){
-  var stream = gulp.src("src/config/**/*")
-  .pipe(gulp.dest('dist/config'));
-  return stream;
-});
-gulp.task("copyPublic",function(){
-  var stream = gulp.src("src/public/**/*")
-  .pipe(gulp.dest('dist/public'));
-  return stream;
+	var stream = gulp.src("src/config/**/*")
+	.pipe(gulp.dest('dist/config'));
+	return stream;
 });
 
 gulp.task("copyAsst",function(){
-  var stream = gulp.src("src/asst/**/*")
-  .pipe(gulp.dest('dist/asst'));
-  return stream;
+	var stream = gulp.src("src/asst/**/*")
+	.pipe(gulp.dest('dist/asst'));
+	return stream;
+});
+// copy views
+gulp.task("copyViews",function(){
+	var stream = gulp.src("src/views/**/*")
+	.pipe(gulp.dest('dist/views'));
+	return stream;
 });
 
-gulp.task("copyIndex",function(){
-  var stream = gulp.src("src/public/index.html")
-  .pipe(rename("index.ejs"))
-  .pipe(gulp.dest('dist/views'))
-  return stream;
+// 前端代码处理
+gulp.task("copyPublic",function(){
+	var stream = gulp.src("src/public/**/*")
+	.pipe(gulp.dest('dist/public'));
+	return stream;
 });
 gulp.task("delIndex",function(){
-  var stream = gulp.src("dist/public/index.html")
-  .pipe(clean());
-  return stream;
+	var stream = gulp.src("dist/public/index.html")
+	.pipe(clean());
+	return stream;
 });
-gulp.task("copyViews",function(){
-  var stream = gulp.src("src/views/**/*")
-  .pipe(gulp.dest('dist/views'));
-  return stream;
-});
-gulp.task("copyTemplate",function(){
-  var stream = gulp.src("src/template/**/*")
-  .pipe(gulp.dest('dist/template'));
-  return stream;
+gulp.task("copyIndex",function(){
+	var stream = gulp.src("src/public/index.html")
+	.pipe(rename("index.ejs"))
+	.pipe(gulp.dest('dist/views'))
+	return stream;
 });
 
-// gulp.task("copyJs",function(){
-//   var stream = gulp.src("src/**/*.js")
-//   // .pipe(uglify())
-//   .pipe(gulp.dest("dist"));
-//   return stream;
-// });
-
+// coffee to js
 gulp.task("coffee",function(){
-  var stream = gulp.src("src/**/*.coffee")
-  .pipe(coffee())
-  // .pipe(uglify())
-  .pipe(gulp.dest('dist'));
-  return stream;
+	var stream = gulp.src("src/**/*.coffee")
+	.pipe(coffee())
+	.pipe(uglify())
+	.pipe(gulp.dest('dist'));
+	return stream;
 });
 
+// watch coffee change
 gulp.task("coffee:w", ["coffee"], function () {
-    watch = gulp.watch("src/**/*.coffee");
-    watch.on("change",function(event){
-      file = event.path
-      dirname = path.dirname(file);
-      dirname = dirname.replace(__dirname,"").replace("/src","dist")
-      gulp.src(event.path)
-      .pipe(coffee())
-      .pipe(gulp.dest(dirname));
-    });
+		watch = gulp.watch("src/**/*.coffee");
+		watch.on("change",function(event){
+			file = event.path
+			dirname = path.dirname(file);
+			dirname = dirname.replace(__dirname,"").replace("/src","dist")
+			gulp.src(event.path)
+			.pipe(coffee())
+			.pipe(gulp.dest(dirname));
+		});
 });
 
+// 起服务
 gulp.task("start",function () {
-  nodemon({
-    script: 'dist/bin/www',
-    ext: 'js json',
-    ignore:[
-      "src/public"
-    ],
-    "events": {
-      "restart": "echo 'display notification \"App restarted due to:\n'$FILENAME'\" with title \"nodemon\"'"
-    },
-    env: { 'NODE_ENV': 'dev' }
-  });
+	nodemon({
+		script: 'dist/bin/www',
+		ext: 'js json',
+		ignore:[
+			"src/public"
+		],
+		"events": {
+			"restart": "echo 'display notification \"App restarted due to:\n'$FILENAME'\" with title \"nodemon\"'"
+		},
+		env: { 'NODE_ENV': 'dev' }
+	});
 });
 
+// 前端处理集合任务
 gulp.task("static",["copyPublic","copyAsst","copyIndex","copyViews"],function(){
-  gulp.start("delIndex");
+	gulp.start("delIndex");
 });
-
+// 产出
 gulp.task("release",["clean"],function(){
-  gulp.start(["copyBin","copyConfig","static","copyTemplate","coffee"]);
+	gulp.start(["copyBin","copyConfig","static","coffee"]);
 });
-
-gulp.task("dev",["copyBin","copyConfig","static","copyTemplate","coffee","coffee:w"],function(){
-  gulp.start("start");
+// 开发
+gulp.task("dev",["copyBin","copyConfig","static","coffee:w"],function(){
+	gulp.start("start");
 });
